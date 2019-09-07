4994109fce45e89e3bfe6c28bd8adb55efafeab2
ohhyogeun <ohhyogeun@khu.ac.kr>
2019-09-07 18:27:52 +0900

    190907 18:27

--- tensorflow_test/__init__.py
+++ tensorflow_test/__init__.py
@@ -1,4 +1,14 @@
 from tensorflow_test.mammal import Mammal    # 교재 p146
+from tensorflow_test.word_sequence import WordSequence
+from tensorflow_test.naive_bayes import NaiveBayes
+from tensorflow_test.web_crawler import WebCrawler
+from tensorflow_test.mail_checker_ctrl import MailCheckerController
 if __name__ == '__main__':
-    t = Mammal()
-    t.execute()
+    # Mammal.execute()
+    # WordSequence.execute()
+    # t = WebCrawler.create_model()
+    # nb = NaiveBayes()
+    # nb.train('./data/review_train.csv')
+    # print(nb.classify('내 인생에서 최고의 영화'))
+    ctrl = MailCheckerController()
+    ctrl.run()
