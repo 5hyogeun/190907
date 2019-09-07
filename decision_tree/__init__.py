4c69fb6fcb6ff875a554c8dc382f0be8c60f7672
ohhyogeun <ohhyogeun@khu.ac.kr>
2019-09-07 13:18:06 +0900

    190907 13:18

--- decision_tree/__init__.py
+++ decision_tree/__init__.py
@@ -1,38 +1,9 @@
-# 교재 p156
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-import mglearn
-import numpy as np
+from decision_tree.ram_price import RamPrice
+from decision_tree.breast_cancer import BreastCancer
+from decision_tree.iris import Iris
 
-ram_price = pd.read_csv(os.path.join(mglearn.datasets.DATA_PATH, "ram_price.csv"))
-plt.semilogy(ram_price.date, ram_price.price)
-plt.xlabel("년")
-plt.ylabel("가격")
-# plt.show()
-
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import LinearRegression
-data_train = ram_price[ram_price['date'] < 2000] # 2000년 기준
-data_test = ram_price[ram_price['date'] >= 2000] # 지도 학습
-x_train = data_train['date'][:, np.newaxis] # train data를 1열로 만든다
-y_train = np.log(data_train['price'])
-tree =DecisionTreeRegressor().fit(x_train, y_train)
-lr = LinearRegression().fit(x_train, y_train)
-# test 는 모든 데이터에 대해 적용한다
-x_all = ram_price['date'].values.reshape(-1, 1) # x_all을 1열로 만든다
-pred_tree = tree.predict(x_all)
-price_tree = np.exp(pred_tree) # Log 값 되돌리기
-pred_lr = lr.predict(x_all)
-price_lr = np.exp(pred_lr) # Log 값 되돌리기
-
-plt.semilogy(ram_price['date'], pred_tree,
-             label = "TREE PREDICT", ls='-', dashes=(2,1))
-plt.semilogy(ram_price['date'], pred_lr,
-             label = "LINEAR REGRESSION", ls=':')
-plt.semilogy(data_train['date'], data_train['price'], label='TRAIN DATA', alpha = 0.4)
-plt.semilogy(data_test['date'], data_test['price'], label='TEST DATA')
-plt.legend(loc=1)
-plt.xlabel('year', size=15)
-plt.ylabel('price', size=15)
-plt.show()
+if __name__ == '__main__':
+    # t = RamPrice()
+    # t = BreastCancer()
+    t = Iris()
+    t.execute()
---
+++ tensorflow_test/__init__.py
@@ -0,0 +1,4 @@
+from tensorflow_test.mammal import Mammal    # 교재 p146
+if __name__ == '__main__':
+    t = Mammal()
+    t.execute()
